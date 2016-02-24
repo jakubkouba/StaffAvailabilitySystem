04603e689f46a387c5fc553723f1233e7c4a2e66
Jakub Adler <adler.jakub@gmial.com>
2016-02-24 14:07:29 +0000

    index page with login and create account link

---
+++ spec/features/index.html.haml_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Index page", type: :feature do
+
+  before :each do
+    visit home_path
+  end
+
+  scenario 'redirect user to create profile page' do
+    create_account_link = find_link('here', { href: new_employees_path })
+    create_account_link.click
+    expect(current_path).to eq(new_employees_path)
+  end
+
+  scenario 'redirect user to login page' do
+    login_link = find_link('Login', { href: login_path })
+    login_link.click
+    expect(current_path).to eq(login_path)
+  end
+
+end
--- spec/views/index/index.html.haml_spec.rb
+++ spec/views/index/index.html.haml_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "index/index.html.haml", type: :view do
-  it 'displays "This is home page"' do
+
+  it 'displays create account link' do
     render
-    expect(rendered).to match /Home page/
+    expect(rendered).to match /class="create-account-link"/
+  end
+
+  it 'displays login link' do
+    render
+    expect(rendered).to match /class="login-link"/
   end
 end
