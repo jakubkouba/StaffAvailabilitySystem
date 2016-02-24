d76988c8d76afa266b8ba5cf2f21bce8f0e7466f
Jakub Adler <adler.jakub@gmial.com>
2016-02-24 17:17:57 +0000

    Employee model migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160224142924) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string   "name",          limit: 64,              null: false
+    t.string   "surname",       limit: 64,              null: false
+    t.datetime "date_of_birth",                         null: false
+    t.string   "email",         limit: 64,              null: false
+    t.integer  "shirt_size",    limit: 2,   default: 0
+    t.string   "password_hash", limit: 128,             null: false
+    t.string   "password_salt", limit: 128,             null: false
+    t.datetime "last_login"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+  end
 
 end
