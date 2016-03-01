ba1dae3dfea89ac8385d925a67c579974589bba1
Jakub Adler <adler.jakub@gmial.com>
2016-03-01 10:34:24 +0000

    Populating DB with default values for StaffType and AccessLevel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+## Populate StaffTypes
+class SasDbPopulate
+
+  class << self
+    def populate_by(model, attr, key)
+      INIT_VALS[key].each { |value| model.find_or_create_by(attr => value) }
+    end
+  end
+end
+
+SasDbPopulate.populate_by(StaffType, :title, :staff_types)
+SasDbPopulate.populate_by(AccessLevel, :title, :access_levels)
