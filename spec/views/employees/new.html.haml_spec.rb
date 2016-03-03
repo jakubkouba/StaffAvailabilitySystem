afbc53b718eb5da35c98f9fe5ce5a86dd17fdcce
Jakub Adler <adler.jakub@gmial.com>
2016-03-03 10:25:58 +0000

    Employee create profile form: saves staff types ids to staff_type_ids attribute; Optionally renders access levels checkboxes if present (for admin)

--- spec/views/employees/new.html.haml_spec.rb
+++ spec/views/employees/new.html.haml_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "employees/new.html.haml", type: :view do
   before(:each) do
     assign(:employee, Employee.new)
     assign(:shirt_sizes, INIT_VALS[:shirt_sizes].map { |size| [size, size]})
+    assign(:staff_types, StaffType.all)
   end
 
   it 'displays "Create Profile"' do
@@ -12,10 +13,28 @@ RSpec.describe "employees/new.html.haml", type: :view do
     expect(rendered).to match /Create Profile/
   end
 
-  it 'displays Profile form' do
-    render :partial => 'profile_form.html.haml'
-    expect(rendered).to match /id="new_employee"/
-  end
+  context 'create Profile Form' do
 
+    it 'renders form' do
+      render
+      expect(rendered).to match /create-profile-form/i
+    end
 
+    it 'displays shirt sizes' do
+      render
+      expect(rendered).to match /shirt size/i
+    end
+
+    it 'displays staff types' do
+      render
+      expect(rendered).to match /staff_type/i
+    end
+
+    it 'displays access levels' do
+      assign(:access_levels, AccessLevel.all)
+      render
+      expect(rendered).to match /authorization/i
+    end
+
+    end
 end
