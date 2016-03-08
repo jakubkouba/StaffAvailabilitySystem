bb7bee2e200bc7844c0480de137af1fb484ce1e1
Jakub Adler <adler.jakub@gmial.com>
2016-03-08 11:25:27 +0000

    login form set up

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
+  describe "GET #new" do
+    it "returns http success and renders new template" do
+      get :new
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
+
+  # describe "GET #create" do
+  #   it "returns http success" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe "GET #destroy" do
+  #   it "returns http success" do
+  #     get :destroy
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
--- spec/views/sessions/new.html.haml_spec.rb
+++ spec/views/sessions/new.html.haml_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "sessions/create.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "sessions/new.html.haml", type: :view do
+  context 'Login form' do
+    it 'displays email and password field' do
+      render
+      expect(rendered).to have_field('email', type: 'email')
+      expect(rendered).to have_field('password')
+      expect(rendered).to have_button('Log In')
+    end
+  end
 end
