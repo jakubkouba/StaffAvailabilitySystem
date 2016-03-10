2e894df7f38ca1eae5e09dfb3dfc36b97fe8bfe0
Jakub Adler <adler.jakub@gmial.com>
2016-03-10 20:08:15 +0000

    Display errors messages on create profile form

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,8 +1,33 @@
 module EmployeesHelper
 
-  def form_field_box(title)
-    haml_tag :div, class: "field #{title} columns small-12 medium-6 large-6" do
+  def form_field_box(field, errors)
+    class_name = field.to_s.gsub('_', '-')
+    haml_tag :div, class: "field #{class_name} columns small-12 medium-6 large-6" do
       yield
+      if errors[field]
+        haml_tag :p, class: "help-text error #{class_name}-error" do
+          haml_concat errors[field].first
+        end
+      end
+    end
+  end
+
+  def build_text_field(form_builder_obj, attr)
+    html        = ''
+    placeholder = PROFILE_FORM_FIELD_DESCRIPTIONS[attr][:placeholder].nil? ? {}: { placeholder: PROFILE_FORM_FIELD_DESCRIPTIONS[attr][:placeholder] }
+    html << form_builder_obj.send(:label, attr)
+    html << form_builder_obj.send(:text_field, attr, placeholder)
+    unless PROFILE_FORM_FIELD_DESCRIPTIONS[attr][:help_text].nil?
+      html << capture_haml do
+                help_text(PROFILE_FORM_FIELD_DESCRIPTIONS[attr][:help_text])
+              end
+    end
+    html.html_safe
+  end
+
+  def help_text(text)
+    haml_tag :p, class: 'help-text' do
+      haml_concat text
     end
   end
 
