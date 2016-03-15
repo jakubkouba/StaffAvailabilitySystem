a3f3a3470fa6ca1265f2d4fe9026e4c164f04d78
Jakub Adler <adler.jakub@gmial.com>
2016-03-15 12:48:42 +0000

    filed description for create profile form. It'll be replaced with i18n

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -3,3 +3,5 @@
 INIT_VALS = YAML.load_file(Rails.root.join('config/app/init_values.yaml'))
 
 EMPLOYEE_DEFAULT_ACCESS_LEVEL = INIT_VALS[:access_levels][-1]
+
+PROFILE_FORM_FIELD_DESCRIPTIONS = YAML.load_file(Rails.root.join('config/app/profile_form_field_description.yaml'))
