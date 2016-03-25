517b77d148c2b6fe8b2b4e12402b17741a5f00d8
Jakub Adler <adler.jakub@gmial.com>
2016-03-25 21:00:54 +0000

    update availabilities on model

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,11 +2,10 @@
 #
 # Table name: availabilities
 #
-#  id          :integer          not null, primary key
 #  employee_id :integer
 #  day         :date             not null
-#  time_from   :datetime         not null
-#  time_to     :datetime         not null
+#  time_from   :time             not null
+#  time_to     :time             not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
@@ -22,4 +21,46 @@ class Availability < ActiveRecord::Base
             :time_from,
             :time_to,
             presence: true
+
+
+  def self.update(new_availabilities, employee_id)
+    availability_collection = self.create_collection(new_availabilities, employee_id)
+    if availability_collection.any?
+      Availability.where(employee_id: employee_id).delete_all
+      availability_collection.each { |availability| availability.save }
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  ##
+  # Create collection of single availability objects
+  #
+  # @param availabilities params['available-times']
+  def self.create_collection(availabilities, employee_id)
+    collection = []
+    availabilities.each do |date, times|
+      availability = Availability.new({ employee_id: employee_id,
+                                        day:         date,
+                                        time_from:   self.create_time_obj(times['from']),
+                                        time_to:     self.create_time_obj(times['to'])
+                                      })
+
+      collection << availability if availability.valid?
+    end
+    collection
+  end
+
+  def self.create_time_obj(time)
+    time = time.split(':')
+    if time.size == 2
+      time.each { |time_fragment| return nil unless time_fragment.match(/\d{2}/) }
+      hour, minute = time[0], time[1]
+      hour > '24' || minute > '60' ? nil : Time.new(0, 1, 1, hour, minute).strftime("%H:%M:%S")
+    end
+  end
+
 end
--- spec/models/availability_spec.rb
+++ spec/models/availability_spec.rb
@@ -21,31 +21,98 @@ RSpec.describe Availability, type: :model do
 
   let(:availability) { build(:availability) }
 
+  before do
+    @employee = create(:post_request_employee)
+
+    @available_times = {
+        "2016-03-24" => {
+            "from" => '13:45',
+            "to"   => '23:30'
+        },
+        "2016-03-25" => {
+            "from" => '07:45',
+            "to"   => '23:30'
+        }
+    }
+
+    @available_times_invalid = {
+        "2016-03-24" => {
+            "from" => 'invalid',
+            "to"   => '23:30'
+        },
+        "2016-03-25" => {
+            "from" => 'invalid',
+            "to"   => '23:30'
+        }
+    }
+  end
+
   it_is_expected_to_validate_presence_for(
       :day,
       :time_from,
       :time_to
   )
 
-  describe "Save availability" do
+  it "creates Time object from string" do
+    expect(described_class.create_time_obj(@available_times["2016-03-24"]["from"])).to match /\d{2}:\d{2}:00/
+  end
 
-    let(:availability) { build(:availability) }
-
-    context "with invalid attributes" do
-      it "fails" do
-        [:day, :time_from, :time_to].each do |attr|
-          availability.send("#{attr}=", 'invalid_datetime')
-          expect(availability.save).to be_falsey
-        end
-      end
-    end
-
-    context "with valid attributes" do
-      it "passes" do
-        expect { availability.save }.to change(Availability, :count).by(1)
-      end
+  it "fails to crate Time object from invalid string" do
+    %w[ 6:30 70:45 10:99 'invalid' ].each do |time|
+      expect(Availability.create_time_obj(time)).to be_nil
     end
   end
 
+  describe "::create_collection" do
+
+    context "with invalid request params" do
+
+      it "returns empty array" do
+        expect(Availability.create_collection(@available_times_invalid, @employee.id)).to be_empty
+      end
+
+    end
+
+    context "with valid request params" do
+
+      it "creates availability collection" do
+        collection = Availability.create_collection(@available_times, @employee.id)
+        expect(collection).to be_instance_of(Array)
+        expect(collection.size).to eq 2
+      end
+
+    end
+
+  end
+
+
+
+  describe "::update" do
+
+    context "with valid request params" do
+
+      it "updates availability" do
+        expect { Availability.update(@available_times, @employee.id) }.to change(Availability, :count).by(@available_times.size)
+      end
+
+      it "update correct time" do
+        Availability.update(@available_times, @employee.id)
+        availability = Availability.find_by_day('2016-03-24')
+        expect(availability.time_from.to_formatted_s(:time)).to eq @available_times['2016-03-24']["from"]
+
+      end
+
+    end
+
+    context "with invalid request params" do
+
+      it "returns false" do
+        result = Availability.update(@available_times_invalid, @employee.id)
+        expect(result).to be_falsey
+      end
+
+    end
+
+  end
 
 end
