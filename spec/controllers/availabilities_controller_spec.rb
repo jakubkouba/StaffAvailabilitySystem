af64d6a63e57a4c7d68e356fcd208ec7b063c470
Jakub Adler <adler.jakub@gmial.com>
2016-04-27 18:07:41 +0100

    AvaliabilitiesController#show facade assigned

--- spec/controllers/availabilities_controller_spec.rb
+++ spec/controllers/availabilities_controller_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe AvailabilitiesController, type: :controller do
       @employee_id = login_employee
       get :show, nil, employee_id: @employee_id
     end
-    
+
     it "assigns current date to @current_date" do
-      expect(assigns(:current_date)).to be_an_instance_of(Date)
+      expect(assigns(:availabilities)).to be_an_instance_of(Facades::Availabilities)
     end
 
   end
