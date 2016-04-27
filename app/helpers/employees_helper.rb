f588a668f54c94444094f5b3585d7d59e16eb19d
Jakub Adler <adler.jakub@gmial.com>
2016-04-27 18:02:09 +0100

    DB Schema info added

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: employees
+#
+#  id            :integer          not null, primary key
+#  name          :string(64)       not null
+#  surname       :string(64)       not null
+#  date_of_birth :date             not null
+#  email         :string(64)       not null
+#  shirt_size    :integer          default(0)
+#  password_hash :string(128)      not null
+#  password_salt :string(128)      not null
+#  last_login    :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 module EmployeesHelper
 
   def form_field_box(field, errors)
