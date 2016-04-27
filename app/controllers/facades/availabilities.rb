7ada27f1a424efd6019374c553d6ad0d442e2b6f
Jakub Adler <adler.jakub@gmial.com>
2016-04-27 13:24:30 +0100

    Availability helper class added to Facades::Availabilities

--- app/controllers/facades/availabilities.rb
+++ app/controllers/facades/availabilities.rb
@@ -1,6 +1,9 @@
 module Facades
   class Availabilities
 
+    attr_reader :current_date,
+                :availability
+
     def initialize(availabilities, date = Date.today)
       @current_date   = date
       @availabilities = availabilities
@@ -21,12 +24,31 @@ module Facades
         selected = availability if date == availability.day
       end
 
-      {
-          disabled:  selected ? false : true,
-          date:      date.to_formatted_s(:date),
-          time_from: selected ? selected.time_from.to_formatted_s(:time) : '00:00',
-          time_to:   selected ? selected.time_to.to_formatted_s(:time) : '00:00'
-      }
+      @availability = AvailableRow.new(selected, date).view_attributes
+
+    end
+
+    class AvailableRow
+
+      attr_accessor :view_attributes
+
+      def initialize(availability, date)
+        @view_attributes = {
+            disabled:  availability ? false : true,
+            date:      date.to_formatted_s(:date),
+            time_from: availability ? availability.time_from.to_formatted_s(:time) : '00:00',
+            time_to:   availability ? availability.time_to.to_formatted_s(:time) : '00:00'
+        }
+      end
+
+      def time(prefix)
+        {
+            prefix:   prefix,
+            disabled: @availability[:disabled],
+            date:     @availability[:date],
+            time:     @availability["time_#{prefix}".to_sym]
+        }
+      end
 
     end
 
